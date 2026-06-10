67878564eb800b18c64868f0d21fba2016ebeeb2
Petrick Martins <petrickmsilva@gmail.com>
2026-06-10 16:13:04 -0300

    Update financeiro.py

--- financeiro.py
+++ financeiro.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, session, redirect, url_for
 from app import get_db_connection
+import pandas as pd
 import os
 
 bp_financeiro = Blueprint('financeiro', __name__)
@@ -9,8 +10,10 @@ def pagina_financeiro():
     if 'usuario_logado' not in session: 
         return redirect(url_for('tela_login'))
     
-    # FORÇA A LISTAGEM NO LOG
-    lista_arquivos = os.listdir('.')
-    print(f"DEBUG_PETRICK: Arquivos na pasta: {lista_arquivos}")
+    # Lista arquivos que terminam com .csv OU .xlsx
+    arquivos = [f for f in os.listdir('.') if f.endswith(('.csv', '.xlsx'))]
+    
+    # DEBUG: Vamos ver no log do Render o que ele achou
+    print(f"DEBUG_PETRICK: Arquivos encontrados: {arquivos}")
     
     return render_template('financeiro.html', nome_sócio=session.get('nome_exibicao', 'Sócio'))
