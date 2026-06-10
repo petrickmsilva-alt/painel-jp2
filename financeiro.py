5736d598b079a7c03fc25def6cc287b9d8848de6
Petrick Martins <petrickmsilva@gmail.com>
2026-06-10 14:22:04 -0300

    Create financeiro.py

---
+++ financeiro.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template, session, redirect, url_for
+
+bp_financeiro = Blueprint('financeiro', __name__)
+
+@bp_financeiro.route('/financeiro')
+def pagina_financeiro():
+    if 'usuario_logado' not in session: return redirect(url_for('tela_login'))
+    return render_template('financeiro.html', nome_sócio=session.get('nome_exibicao', 'Sócio'))
